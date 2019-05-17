b964701157b4816fc302fc121af0514b2f1a327e
Steffen André Langnes <steffenl.sw@gmail.com>
2019-05-17 12:44:12 +0200

    Update SDK and download from S3

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,10 @@
 import re
 from conans import ConanFile, CMake, tools
+import boto3
 
 class SteamworksConan(ConanFile):
     name = "Steamworks"
-    version = "1.42"
+    version = "1.44"
     url = "https://github.com/SteffenL/conan-steamworks.git"
     description = "Steamworks SDK"
     settings = {
@@ -12,13 +13,29 @@ class SteamworksConan(ConanFile):
     }
     license = "Apache-2.0"
 
+    _checksums = {
+        "1.42": "7695f8e183bef16dc2e663ffbdfad2248ae266bce8ff42066a3e88e1d54f0f42",
+        "1.43": "600b4e8a098fe4c9f496b39889860b4932253e6a68f3243c408d621ac7f3ebdd",
+        "1.44": "6f7b85bfe1bf032f1a1d743db30c43d31d04010e127e134e9b5a98b8ebe37372"
+    }
+    _s3_region = "eu-north-1"
+    _s3_bucket = "private.resources.development.langnes"
+    _s3_path = "thirdparty/steamworks/steamworks_sdk_%s.zip" % version.replace(".", "")
+
     def configure(self):
         if self.settings.os == "Macos" and self.settings.arch != "x86":
             raise ValueError("Only x86 is supported for macOS")
 
     def build(self):
-        url = "https://s3-eu-west-1.amazonaws.com/development.langnes/thirdparty/steamworks/steamworks_sdk_%s.zip" % self.version.replace(".", "")
-        tools.get(url)
+        checksum = SteamworksConan._checksums[self.version]
+        akid = tools.get_env("LANGNES_AWS_ACCESS_KEY_ID")
+        sak = tools.get_env("LANGNES_AWS_SECRET_ACCESS_KEY")
+
+        local_path = "sdk.zip"
+        s3 = boto3.client("s3", region_name=SteamworksConan._s3_region, aws_access_key_id=akid, aws_secret_access_key=sak)
+        s3.download_file(SteamworksConan._s3_bucket, SteamworksConan._s3_path, local_path)
+        tools.check_sha256(local_path, checksum)
+        tools.unzip(local_path)
 
     def package(self):
         self.copy("*.h", "include", "sdk/public")
